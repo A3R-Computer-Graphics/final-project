0e4b9ae8987ab5d052ed4df931f400b1a4e9e915
Albertus Angga Raharja <albertus.angga@ui.ac.id>
2020-04-17 18:24:26 +0700

    Connect parent-children for every node and refactor style

--- experiment-3/model.js
+++ experiment-3/model.js
@@ -1,24 +1,39 @@
 class Model {
-  constructor(name, origin, location, rotation, scale, material, node) {
+  constructor({
+    name,
+    origin,
+    location,
+    rotation,
+    scale,
+    material,
+    bufferStartIndex,
+    vertexCount,
+    parentName,
+  }) {
     this.name = name;
     this.origin = [...origin];
     this.location = [...location];
     this.rotation = [...rotation];
     this.scale = [...scale];
-
     this.material = material;
-    this.node = node;
-    this.bufferStartIndex = 0;
-    this.vertexCount = 0;
 
-    /**
-     * Transformation matrix computed from loc, rot, and scale.
-     */
+    this.bufferStartIndex = bufferStartIndex;
+    this.vertexCount = vertexCount;
+
+    this.node = ObjectNode.getOrCreate(name).updateWith({
+      model: this,
+      parent: !!parentName
+        ? ObjectNode.getOrCreate(parentName)
+        : undefined,
+    });
+
+    // Transformation matrix computed from loc, rot, and scale.
     this.transformationMatrix = m4.identity();
-    /**
-     * Full transformation matrix, computed from this model as well as from the parent.
-     */
+
+    // Full transformation matrix, computed from this model as well as from the parent.
     this.fullTransformMatrix = m4.identity();
+
+    this.updateMatrices();
   }
 
   getParentName() {
@@ -30,12 +45,12 @@ class Model {
       this.location[0] + this.origin[0],
       this.location[1] + this.origin[1],
       this.location[2] + this.origin[2]
-    )
-    mat = m4.scale(mat, this.scale[0], this.scale[1], this.scale[2])
-    mat = m4.xRotate(mat, degToRad(this.rotation[0]))
-    mat = m4.yRotate(mat, degToRad(this.rotation[1]))
-    mat = m4.zRotate(mat, degToRad(this.rotation[2]))
-    mat = m4.translate(mat, this.origin[0], this.origin[1], this.origin[2])
+    );
+    mat = m4.scale(mat, this.scale[0], this.scale[1], this.scale[2]);
+    mat = m4.xRotate(mat, degToRad(this.rotation[0]));
+    mat = m4.yRotate(mat, degToRad(this.rotation[1]));
+    mat = m4.zRotate(mat, degToRad(this.rotation[2]));
+    mat = m4.translate(mat, this.origin[0], this.origin[1], this.origin[2]);
     this.transformationMatrix = mat;
   }
 
@@ -44,17 +59,20 @@ class Model {
    * The transformation matrix need to be updated first before performing
    * this operation.
    */
-
   updateFullTransformationMatrix() {
     // objectNodesList and objectNameToId is a global variable
     // I know, it looks dirty to call those variables here
     // but this is the fastest way to call it.
 
-    var parentName = this.getParentName()
-    if (parentName) {
-      var parentNode = objectNodesList[objectNameToId[parentName]];
+    // If the node already has parent and the model is already initialized
+    if (this.node.hasParent && !!this.node.parent.model) {
+      var parentNode = this.node.parent;
+      console.log('parent: ', parentNode);
       var parentMatrix = parentNode.model.fullTransformMatrix;
-      this.fullTransformMatrix = m4.multiply(parentMatrix, this.transformationMatrix);
+      this.fullTransformMatrix = m4.multiply(
+        parentMatrix,
+        this.transformationMatrix
+      );
     } else {
       this.fullTransformMatrix = this.transformationMatrix;
     }
@@ -71,14 +89,14 @@ class Model {
 
   deltaGeneral(arr, x, y, z) {
     if (x.length === 3) {
-      y = x[1]
-      z = x[2]
-      x = x[0]
+      y = x[1];
+      z = x[2];
+      x = x[0];
     }
     arr[0] += x || 0;
     arr[1] += y || 0;
     arr[2] += z || 0;
-    
+
     this.updateMatrices();
   }
 
@@ -96,5 +114,34 @@ class Model {
 
   deltaLocation(x, y, z) {
     this.deltaGeneral(this.location, x, y, z);
+  }
+
+  /**
+   * Generic function to render 3D object, given model matrix,
+   * Phong parameters (ambient, diffuse, specular, and shininess),
+   * and object's buffer ranges.
+   *
+   * NOTE: The Phong variables need to be flattened first!
+   * Since materials change rarely, we can store its flattened product
+   * to avoid calling `flatten()` every time an object is rendered.
+   */
+  render(gl) {
+    const {
+      ambientProduct,
+      diffuseProduct,
+      specularProduct,
+      shininess,
+    } = this.material;
+    gl.uniform4fv(gl.ambientLoc, ambientProduct);
+    gl.uniform4fv(gl.diffuseLoc, diffuseProduct);
+    gl.uniform4fv(gl.specularLoc, specularProduct);
+    gl.uniform1f(gl.shininessLoc, shininess);
+
+    gl.uniformMatrix4fv(
+      gl.modelMatrixLoc,
+      false,
+      flatten(this.fullTransformMatrix)
+    );
+    gl.drawArrays(gl.TRIANGLES, this.bufferStartIndex, this.vertexCount);
   }
 }
