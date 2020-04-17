b7d29ecfd17b87a0cad20f4044d4c765fec583ce
Rachmat Ridwan <rachmat.ridwan@ui.ac.id>
2020-04-18 05:56:58 +0700

    Add Link object

--- temp/objects-data.js
+++ temp/objects-data.js
@@ -1,35 +1,162 @@
 var objects_info = {
-    "Suzanne": {
-        "location": [0.0, 0.0, 1.6],
-        "rotation": [0.0, 0.0, 0.0],
-        "scale": [1.0, 1.0, 1.0],
-        "material_name": "pink",
-        "parent": "arm.upper"
-    },
-    "axis": {
-        "location": [1.5, -1.0, -0.2],
-        "rotation": [0.0, 0.0, 0.0],
-        "scale": [0.5, 0.5, 0.5],
-        "material_name": "pink"
-    },
-    "arm.base": {
-        "location": [0.0, 0.0, 0.0],
-        "rotation": [0.0, 0.0, 0.0],
-        "scale": [1.0, 1.0, 1.0],
-        "material_name": "arm.floor"
-    },
-    "arm.lower": {
-        "location": [0.0, 0.0, 0.2],
-        "rotation": [0.0, 0.0, 0.0],
-        "scale": [1.0, 1.0, 1.0],
-        "material_name": "arm.bot",
-        "parent": "arm.base"
-    },
-    "arm.upper": {
-        "location": [0.0, 0.0, 1.6],
-        "rotation": [0.0, 0.0, 0.0],
-        "scale": [1.0, 1.0, 1.0],
-        "material_name": "arm.top",
-        "parent": "arm.lower"
-    }
+  "head": {
+      "location": [-0.001, -0.0, 0.645],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "skin",
+      "parent": "body"
+  },
+  "eye": {
+      "location": [-0.0, -0.992, 0.857],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "eye",
+      "parent": "head"
+  },
+  "mouth": {
+      "location": [-0.0, -0.992, 0.427],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "mouth & eyebrow",
+      "parent": "head"
+  },
+  "body": {
+      "location": [0.0, 0.0, 0.0],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "cloth green"
+  },
+  "eyebrow": {
+      "location": [0.0, -0.992, 1.342],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "mouth & eyebrow",
+      "parent": "head"
+  },
+  "hair": {
+      "location": [0.126, -1.446, 2.292],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "hair",
+      "parent": "head"
+  },
+  "cap": {
+      "location": [0.0, 0.425, 1.273],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "cloth green",
+      "parent": "head"
+  },
+  "belt_strap": {
+      "location": [-0.001, -0.0, -0.308],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "belt",
+      "parent": "body"
+  },
+  "belt_buckle": {
+      "location": [-0.001, -0.309, -0.308],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "belt",
+      "parent": "body"
+  },
+  "arm.L": {
+      "location": [0.852, -0.382, 0.286],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "sleeve",
+      "parent": "body"
+  },
+  "palm.L": {
+      "location": [0.0, -1.081, -0.011],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "skin",
+      "parent": "arm.L"
+  },
+  "sword_crossguard": {
+      "location": [0.0, 0.0, 0.396],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "sword crossguard",
+      "parent": "sword_grip"
+  },
+  "sword_grip": {
+      "location": [0.0, 0.0, -0.009],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "sword grip",
+      "parent": "palm.L"
+  },
+  "leg.L": {
+      "location": [0.317, -0.016, -0.601],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "pants",
+      "parent": "body"
+  },
+  "chest": {
+      "location": [-0.001, -0.506, 0.583],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "skin",
+      "parent": "body"
+  },
+  "boot_shaft.L": {
+      "location": [0.0, 0.0, -0.657],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "shoe",
+      "parent": "leg.L"
+  },
+  "boot_lower.L": {
+      "location": [-0.001, 0.162, -0.23],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "shoe",
+      "parent": "boot_shaft.L"
+  },
+  "sword_blade": {
+      "location": [0.028, 0.0, 1.268],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "sword blade",
+      "parent": "sword_grip"
+  },
+  "arm.R": {
+      "location": [-0.855, -0.382, 0.286],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "sleeve",
+      "parent": "body"
+  },
+  "palm.R": {
+      "location": [0.0, -1.081, -0.011],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "skin",
+      "parent": "arm.R"
+  },
+  "leg.R": {
+      "location": [-0.318, -0.016, -0.601],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "pants",
+      "parent": "body"
+  },
+  "boot_shaft.R": {
+      "location": [0.0, 0.0, -0.657],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "shoe",
+      "parent": "leg.R"
+  },
+  "boot_lower.R": {
+      "location": [0.001, 0.162, -0.23],
+      "rotation": [0.0, 0.0, 0.0],
+      "scale": [1.0, 1.0, 1.0],
+      "material_name": "shoe",
+      "parent": "boot_shaft.R"
+  }
 }
