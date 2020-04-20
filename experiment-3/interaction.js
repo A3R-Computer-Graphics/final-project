74f8cb25731b821e32cb8b01007a3c7fde2f058d
Rachmat Ridwan <rachmat.ridwan@ui.ac.id>
2020-04-20 15:11:19 +0700

    Extract functions to interpolate speed parameter

--- experiment-3/interaction.js
+++ experiment-3/interaction.js
@@ -22,6 +22,16 @@ function parsePropertyString(stringPropertyName) {
   }
 }
 
+function interpolateExponentially(start, end, t) {
+  let multiplier = Math.log(end / start);
+  return start * Math.exp(multiplier * (t - start) / (end - start));
+}
+
+function interpolateLogarithmatically(start, end, t) {
+  let multiplier = Math.log(end / start);
+  return Math.log(t / start) / multiplier * (end - start) + start;
+}
+
 window.addEventListener('load', function () {
   this.document.querySelectorAll('input[type="range"]').forEach(elem => {
     const sliderName = elem.getAttribute('name')
@@ -39,20 +49,20 @@ window.addEventListener('load', function () {
     })
   })
 
-  let speedSlider = document.querySelector('input[name="speed"]')
+  let SPEED_MIN = 0.05;
+  let SPEED_MAX = 4;
+  let speedSlider = document.querySelector('input[name="speed"]');
+  let speedValueDisplay = speedSlider.parentElement.querySelector('.slider-value');
+
   speedSlider.addEventListener('input', event => {
-    let textVal = event.target.parentElement.querySelector('.slider-value')
     let value = parseFloat(event.target.value);
-    let multiplier = Math.log(4 / 0.05);
-    value = 0.05 * Math.exp(multiplier * (value - 0.05) / (4 - 0.05));
-    textVal.textContent = Math.round(value * 100) / 100;
+    value = interpolateExponentially(SPEED_MIN, SPEED_MAX, value);
+    speedValueDisplay.innerText = Math.round(value * 100) + '%';
     updateSpeed(value);
   })
 
-  let multiplier = Math.log(4 / 0.05);
-  let speedSliderValue = Math.log(speed / 0.05) / multiplier * (4 - 0.05) + 0.05;
-  speedSlider.value = speedSliderValue;
-  speedSliderValue = Math.round(speedSliderValue * 100) / 100;
-  speedSlider.parentElement.querySelector('.slider-value').textContent = speed;
-
+  // Init slider position from inverse of exponential (logarithm)
+  let sliderInitValue = interpolateLogarithmatically(SPEED_MIN, SPEED_MAX, speed);
+  speedSlider.value = sliderInitValue;
+  speedValueDisplay.innerText = Math.round(speed * 100) + '%';
 })
