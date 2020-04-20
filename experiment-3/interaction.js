7c31801a948b4d08675c6043b461f743b4f61f6e
Rachmat Ridwan <rachmat.ridwan@ui.ac.id>
2020-04-20 07:20:14 +0700

    Extract functions to parse anim property name

--- experiment-3/interaction.js
+++ experiment-3/interaction.js
@@ -1,43 +1,58 @@
-window.addEventListener('load', function() {
+/**
+ * Convert property name in format of string into
+ * dictionary. Supported properties are location, rotation
+ * and scale. If string does not match the format, this returns
+ * undefined.
+ * 
+ * Examples:
+ * `head.rotation.x` returns { modelName: "head", propertyName: "rotation", axisId: 0 }.
+ * 
+ * @param {String} stringPropertyName 
+ */
+function parsePropertyString(stringPropertyName) {
+  const matches = stringPropertyName.match(/^([a-zA-Z_.]+)\.(location|rotation|scale)\.(x|y|z)$/);
+  if (!matches) {
+    return
+  }
 
-    // Query all input sliders
-    this.document.querySelectorAll('input[type="range"]').forEach(elem => {
-        // Get attribute slider name
-        const sliderName = elem.getAttribute('name')
-        
-        // find match
-        const matches = sliderName.match(/^([a-zA-Z_.]+)\.(location|rotation|scale)\.(x|y|z)$/);
-        if (!matches) {
-            return
-        }
+  return {
+    modelName: matches[1],
+    propertyName: matches[2],
+    axisId: ['x', 'y', 'z'].indexOf(matches[3])
+  }
+}
 
-        const objectName = matches[1];
-        const propertyName = matches[2];
-        const axisId = ['x', 'y', 'z'].indexOf(matches[3]);
+window.addEventListener('load', function () {
+  this.document.querySelectorAll('input[type="range"]').forEach(elem => {
+    const sliderName = elem.getAttribute('name')
+    const propertyData = parsePropertyString(sliderName);
+    if (propertyData === undefined) {
+      return
+    }
 
-        // Attach input event listener to this slider
-        elem.addEventListener('input', function(event) {
-            ObjectNode.cache[objectName].model[propertyName][axisId] = parseFloat(event.target.value);
-            ObjectNode.cache[objectName].updateTransformations()
-            let textVal = event.target.parentElement.querySelector('.slider-value')
-            textVal.innerHTML = parseFloat(event.target.value);
-        })
+    const { modelName, propertyName, axisId } = propertyData;
+    elem.addEventListener('input', function (event) {
+      ObjectNode.cache[modelName].model[propertyName][axisId] = parseFloat(event.target.value);
+      ObjectNode.cache[modelName].updateTransformations()
+      let textVal = event.target.parentElement.querySelector('.slider-value')
+      textVal.innerHTML = parseFloat(event.target.value);
     })
+  })
 
-    let speedSlider = document.querySelector('input[name="speed"]')
-    speedSlider.addEventListener('input', event => {
-        let textVal = event.target.parentElement.querySelector('.slider-value')
-        let value = parseFloat(event.target.value);
-        let multiplier = Math.log(4 / 0.05);
-        value = 0.05 * Math.exp(multiplier * (value - 0.05) / (4 - 0.05));
-        textVal.textContent = Math.round(value * 100) / 100;
-        updateSpeed(value);
-    })
-
+  let speedSlider = document.querySelector('input[name="speed"]')
+  speedSlider.addEventListener('input', event => {
+    let textVal = event.target.parentElement.querySelector('.slider-value')
+    let value = parseFloat(event.target.value);
     let multiplier = Math.log(4 / 0.05);
-    let speedSliderValue = Math.log(speed / 0.05) / multiplier * (4 - 0.05) + 0.05;
-    speedSlider.value = speedSliderValue;
-    speedSliderValue = Math.round(speedSliderValue * 100) / 100;
-    speedSlider.parentElement.querySelector('.slider-value').textContent = speed;
-    
+    value = 0.05 * Math.exp(multiplier * (value - 0.05) / (4 - 0.05));
+    textVal.textContent = Math.round(value * 100) / 100;
+    updateSpeed(value);
+  })
+
+  let multiplier = Math.log(4 / 0.05);
+  let speedSliderValue = Math.log(speed / 0.05) / multiplier * (4 - 0.05) + 0.05;
+  speedSlider.value = speedSliderValue;
+  speedSliderValue = Math.round(speedSliderValue * 100) / 100;
+  speedSlider.parentElement.querySelector('.slider-value').textContent = speed;
+
 })
