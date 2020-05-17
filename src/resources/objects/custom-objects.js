b326c80c5338edb5ebf1c9bfc3d6ac6c01e1f568
Albertus Angga <albertusangga.98@gmail.com>
2020-05-17 20:32:18 +0700

    Merge non simple object resources

--- src/resources/objects/custom-objects.js
+++ src/resources/objects/custom-objects.js
@@ -5,6 +5,101 @@
 
 var [LENGTH, WIDTH, HEIGHT] = [10.0, 10.0, 10.0];
 
+objects_info = {
+    ...objects_info,
+    "Cube": {
+        "position": [
+            0.0,
+            0.0,
+            0.0
+        ],
+        "material_name": "cubes",
+        "rotation": [
+            0.0,
+            0.0,
+            0.0
+        ],
+        "scale": [
+            1.0,
+            1.0,
+            1.0
+        ]
+    },
+    "Cube.001": {
+        "position": [
+            2.525,
+            -1.463,
+            -0.45
+        ],
+        "material_name": "cubes",
+        "parent": "Cube",
+        "rotation": [
+            0.0,
+            0.0,
+            0.0
+        ],
+        "scale": [
+            0.5456840991973877,
+            0.5456840991973877,
+            0.5456840991973877
+        ]
+    },
+    "Suzanne": {
+        "position": [
+            0.886,
+            -2.385,
+            1.582
+        ],
+        "material_name": "monkey",
+        "rotation": [
+            0.0,
+            0.0,
+            0.0
+        ],
+        "scale": [
+            0.8567630648612976,
+            0.8567630648612976,
+            0.8567630648612976
+        ]
+    },
+    "floor": {
+        "position": [
+            0.0,
+            -3.0,
+            -1.0
+        ],
+        "material_name": "floor",
+        "rotation": [
+            0.0,
+            0.0,
+            0.0
+        ],
+        "scale": [
+            10,
+            10,
+            10
+        ]
+    },
+    "wall": {
+        "position": [
+            0.0,
+            7.743,
+            -1.0
+        ],
+        "material_name": "floor",
+        "rotation": [
+            90,
+            -0.0,
+            0.0
+        ],
+        "scale": [
+            10,
+            10,
+            10
+        ]
+    }
+}
+
 objects_info.floor = {
   ...objects_info.floor,
   position: [0.0, 0.0, -1.0],
