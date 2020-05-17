27fdda062292f76e9cf77d3e0afd9d3c7bb04595
Albertus Angga <albertusangga.98@gmail.com>
2020-05-17 18:30:40 +0700

    Automate world space calculation with LENGTH,WIDTH,HEIGHT variables

--- src/resources/objects/custom-objects.js
+++ src/resources/objects/custom-objects.js
@@ -1,123 +1,63 @@
-/** 
+/**
  * Declare custom objects that reuses existing vertices/colors here.
  * Make sure to import objects-data.js or objects-data-simple.js first
  */
 
-objects_info = {
-    ...objects_info,
-    "floor": {
-        ...objects_info.floor,
-        "vertices": "floor",
-        "position": [
-            0.0,
-            -3.0,
-            -1.0
-        ],
-        "material_name": "floor",
-        "rotation": [
-            0.0,
-            0.0,
-            0.0
-        ],
-        "scale": [
-            10,
-            10,
-            10
-        ]
-    },
-    "wall": {
-        ...objects_info.wall,
-        "material_name": "floor",
-        "rotation": [
-            90,
-            -0.0,
-            0.0
-        ],
-        "position": [
-            0,
-            7,
-            4.0,
-        ],
-        "scale": [
-            10,
-            5,
-            10
-        ],
-    },
-    "front_wall": {
-        "vertices": "wall",
-        "material_name": "floor",
-        "rotation": [
-            90,
-            0.0,
-            0.0
-        ],
-        "position": [
-            0,
-            -13.0,
-            4.0,
-        ],
-        "scale": [
-            10,
-            5,
-            10
-        ],
-    },
-    "ceiling": {
-        "vertices": "floor",
-        "position": [
-            0.0,
-            -3.0,
-            9.0
-        ],
-        "material_name": "floor",
-        "rotation": [
-            0.0,
-            0.0,
-            0.0
-        ],
-        "scale": [
-            10,
-            10,
-            10
-        ]
-    },
-    "left_wall": {
-        "vertices": "wall",
-        "position": [
-            -10.0,
-            -3.0,
-            4.0
-        ],
-        "material_name": "floor",
-        "rotation": [
-            0,
-            90,
-            0.0
-        ],
-        "scale": [
-            5,
-            10,
-            10
-        ]
-    },
-    "right_wall": {
-        "vertices": "wall",
-        "position": [
-            10.0,
-            -3.0,
-            4.0
-        ],
-        "material_name": "floor",
-        "rotation": [
-            0,
-            90,
-            0.0
-        ],
-        "scale": [
-            5,
-            10,
-            10
-        ]
-    }
-}
+var [LENGTH, WIDTH, HEIGHT] = [10.0, 10.0, 10.0];
+
+objects_info.floor = {
+  ...objects_info.floor,
+  position: [0.0, 0.0, -1.0],
+  scale: [LENGTH, WIDTH, 1],
+};
+
+objects_info.wall = {
+  ...objects_info.wall,
+  vertices: "wall",
+  position: [
+    objects_info.floor.position[0],
+    objects_info.floor.position[1] + WIDTH,
+    objects_info.floor.position[2] + HEIGHT,
+  ],
+  scale: [LENGTH, HEIGHT, WIDTH],
+};
+
+objects_info["left_wall"] = {
+  ...objects_info.wall,
+  vertices: "wall",
+  rotation: [90.0, 0, 90.0],
+  position: [
+    objects_info.floor.position[0] - objects_info.wall.scale[0],
+    objects_info.floor.position[1],
+    objects_info.floor.position[2] + objects_info.wall.scale[1],
+  ],
+  scale: [WIDTH, HEIGHT, LENGTH],
+};
+
+objects_info["ceiling"] = {
+  ...objects_info.floor,
+  vertices: "floor",
+  position: [
+    objects_info.floor.position[0],
+    objects_info.floor.position[1],
+    objects_info.floor.position[2] + objects_info.wall.scale[1] * 2,
+  ],
+};
+
+objects_info["right_wall"] = {
+  ...objects_info.left_wall,
+  position: [
+    objects_info.left_wall.position[0] + objects_info.floor.scale[0] * 2,
+    objects_info.left_wall.position[1],
+    objects_info.left_wall.position[2],
+  ],
+};
+
+objects_info["front_wall"] = {
+  ...objects_info.wall,
+  position: [
+      objects_info.wall.position[0],
+      objects_info.wall.position[1] - WIDTH * 2,
+      objects_info.wall.position[2],
+  ]
+};
