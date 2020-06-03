16c3a77fe2b1d5fc78dd99aa07181f58423dc544
Rachmat Ridwan <rachmat.ridwan72@gmail.com>
2020-06-03 17:17:41 +0700

    Smoothen the image textures

--- src/modules/3d/material.js
+++ src/modules/3d/material.js
@@ -87,6 +87,10 @@ class ImageTextureMaterial extends PhongMaterial {
     gl.bindTexture(gl.TEXTURE_2D, texture)
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
       new Uint8Array([255, 0, 255, 255]))
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT)
 
     this.textureNeedsInitialization = false
 
